b7433c99dc8faabd11095084af6eec689ced734d
n1ck1ng <107306847+n1ck1ng@users.noreply.github.com>
2022-06-20 19:18:13 +0300

    Update kysor.sh

--- KYVE/kysor.sh
+++ KYVE/kysor.sh
@@ -7,6 +7,7 @@ curl -s https://raw.githubusercontent.com/n1ck1ng/files/main/logo.sh | \
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install curl
+sudo apt install git
 curl https://deb.nodesource.com/setup_14.x | sudo bash
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
